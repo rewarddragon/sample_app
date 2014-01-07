da486e0fdb7e3ae8ab7e398210cf26c86b9f13f4
Rich Rardin <rewarddragon@gmail.com>
2014-01-07 15:39:20 +0000

    about to branch for sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates( :email, {presence: true, uniqueness: {case_sensitive: false},
                       format: {with: VALID_EMAIL_REGEX}} )
+  
+
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
